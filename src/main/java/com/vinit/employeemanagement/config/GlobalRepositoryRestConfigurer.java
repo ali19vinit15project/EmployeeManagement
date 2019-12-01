8ffd701fd102938128d784d5e0b07bf473326631
Vinit Patil <vinit.patil@mastek.com>
2019-12-01 19:35:58 +0530

    Cors global level configuration added

---
+++ src/main/java/com/vinit/employeemanagement/config/GlobalRepositoryRestConfigurer.java
@@ -0,0 +1,33 @@
+package com.vinit.employeemanagement.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class GlobalRepositoryRestConfigurer implements RepositoryRestConfigurer {
+
+    @Value("${corsConfig.base-pattern}")
+    private String CORS_BASE_PATTERN;
+
+    @Value("${corsConfig.allowed-origins}")
+    private String ALLOWED_ORIGINS;
+
+    @Value("${corsConfig.allowed-methods}")
+    private String ALLOWED_METHODS;
+
+    @Value("${corsConfig.allowed-headers}")
+    private String ALLOWED_HEADERS;
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+
+        config.getCorsRegistry()//
+                .addMapping(CORS_BASE_PATTERN)//
+                .allowedOrigins(ALLOWED_ORIGINS)//
+                .allowedHeaders(ALLOWED_HEADERS)//
+                .allowedMethods(ALLOWED_METHODS);
+    }
+
+}
--- src/main/java/com/vinit/employeemanagement/dao/EmployeeDao.java
+++ src/main/java/com/vinit/employeemanagement/dao/EmployeeDao.java
@@ -2,11 +2,9 @@ package com.vinit.employeemanagement.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.vinit.employeemanagement.model.Employee;
 
-@CrossOrigin("http://localhost:8080")
 @RepositoryRestResource(collectionResourceRel = "employees", path = "employees")
 public interface EmployeeDao extends JpaRepository<Employee, Long> {
 
