386fbb8e1d4bd5cd11ca1d0cfc762a1f02c6e03b
Asgar25 <asgara75@gmail.com>
2020-01-05 01:19:27 +0530

    show primary key id in response for employee

--- src/main/java/com/vinit/employeemanagement/config/GlobalRepositoryRestConfigurer.java
+++ src/main/java/com/vinit/employeemanagement/config/GlobalRepositoryRestConfigurer.java
@@ -5,29 +5,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
+import com.vinit.employeemanagement.model.Employee;
+
 @Configuration
 public class GlobalRepositoryRestConfigurer implements RepositoryRestConfigurer {
 
-    @Value("${corsConfig.base-pattern}")
-    private String CORS_BASE_PATTERN;
+	@Value("${corsConfig.base-pattern}")
+	private String CORS_BASE_PATTERN;
 
-    @Value("${corsConfig.allowed-origins}")
-    private String ALLOWED_ORIGINS;
+	@Value("${corsConfig.allowed-origins}")
+	private String ALLOWED_ORIGINS;
 
-    @Value("${corsConfig.allowed-methods}")
-    private String ALLOWED_METHODS;
+	@Value("${corsConfig.allowed-methods}")
+	private String ALLOWED_METHODS;
 
-    @Value("${corsConfig.allowed-headers}")
-    private String ALLOWED_HEADERS;
+	@Value("${corsConfig.allowed-headers}")
+	private String ALLOWED_HEADERS;
 
-    @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 
-        config.getCorsRegistry()//
-                .addMapping(CORS_BASE_PATTERN)//
-                .allowedOrigins(ALLOWED_ORIGINS)//
-                .allowedHeaders(ALLOWED_HEADERS)//
-                .allowedMethods(ALLOWED_METHODS);
-    }
+		config.exposeIdsFor(Employee.class);
+
+		config.getCorsRegistry()//
+				.addMapping(CORS_BASE_PATTERN)//
+				.allowedOrigins(ALLOWED_ORIGINS)//
+				.allowedHeaders(ALLOWED_HEADERS)//
+				.allowedMethods(ALLOWED_METHODS);
+	}
 
 }
