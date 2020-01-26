b6b50f5206801734dfc84b682f54dc5faeba79a0
ali19vinit15project <alivinitproject@gmail.com>
2020-01-26 13:22:48 +0530

    Api Employee search Functionality #4
    Example
    employees?firstName=irstNam&lastName=astNam

--- src/main/java/com/vinit/employeemanagement/dao/EmployeeDao.java
+++ src/main/java/com/vinit/employeemanagement/dao/EmployeeDao.java
@@ -1,11 +1,34 @@
 package com.vinit.employeemanagement.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import com.querydsl.core.types.dsl.StringPath;
 import com.vinit.employeemanagement.model.Employee;
+import com.vinit.employeemanagement.model.QEmployee;
 
 @RepositoryRestResource(collectionResourceRel = "employees", path = "employees")
-public interface EmployeeDao extends JpaRepository<Employee, Long> {
+public interface EmployeeDao extends JpaRepository<Employee, Long>, QuerydslPredicateExecutor<Employee>,
+	QuerydslBinderCustomizer<QEmployee> {
 
+    @Override
+    default public void customize(QuerydslBindings bindings, QEmployee emp) {
+
+	// specific properties
+	// bindings.bind(emp.firstName).first((path, value) ->
+	// path.equalsIgnoreCase(value));
+	// bindings.bind(emp.lastName).first((path, value) ->
+	// path.equalsIgnoreCase(value));
+
+	// OR
+	// All properties
+	bindings.bind(String.class).first((StringPath path, String value) -> path.containsIgnoreCase(value));
+
+	// excluding
+	// bindings.excluding(emp.aadhar);
+
+    }
 }
--- src/main/java/com/vinit/employeemanagement/model/Employee.java
+++ src/main/java/com/vinit/employeemanagement/model/Employee.java
@@ -8,12 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
 import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 
@@ -66,7 +64,6 @@ public class Employee {
     @NotBlank
     private String qualification;
 
-    @Lob
     @NotNull
     private String photo;
 
